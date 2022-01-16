d37baff94e552140775732191f0f42e15123d72c
Sidney Burks <sid137@gmail.com>
2022-01-16 17:15:04 +0100

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
